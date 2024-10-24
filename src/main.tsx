fa6d61ae45a7c061cf0c0ca8c99ddd01f50beecf
ccwCCW95 <ccw15111840702@gmail.com>
2024-10-24 19:20:30 +1000

    05/Initialize reset-css

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@
  * @Author: Changwei Cao
  * @Date: 2024-10-23 20:17:15
  * @LastEditors: Changwei Cao
- * @LastEditTime: 2024-10-24 18:57:45
+ * @LastEditTime: 2024-10-24 19:19:06
  * @Description: 
  */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "reset-css"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
