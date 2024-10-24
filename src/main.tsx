f1e5047576901234771b96b51d1533ef7f51f332
ccwCCW95 <ccw15111840702@gmail.com>
2024-10-24 19:47:51 +1000

    06/Setup SCSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@
  * @Author: Changwei Cao
  * @Date: 2024-10-23 20:17:15
  * @LastEditors: Changwei Cao
- * @LastEditTime: 2024-10-24 19:19:06
+ * @LastEditTime: 2024-10-24 19:44:34
  * @Description: 
  */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "reset-css"
+import "./assets/styles/global.scss"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
