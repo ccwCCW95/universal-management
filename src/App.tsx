a124e23723e23fbc9d7d4bfa82241f0a1f2b3a78
ccwCCW95 <ccw15111840702@gmail.com>
2024-10-24 19:01:02 +1000

    Initialize folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
+/*
+ * @Author: Changwei Cao
+ * @Date: 2024-10-23 20:17:15
+ * @LastEditors: Changwei Cao
+ * @LastEditTime: 2024-10-24 18:59:28
+ * @Description: 
+ */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      Super Component
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
+/*
+ * @Author: Changwei Cao
+ * @Date: 2024-10-23 20:17:15
+ * @LastEditors: Changwei Cao
+ * @LastEditTime: 2024-10-24 18:57:45
+ * @Description: 
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
