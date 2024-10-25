10945b4ac785db5cb2442bbf74ea5c37ce05bc76
ccwCCW95 <ccw15111840702@gmail.com>
2024-10-25 20:15:32 +1000

    07 Set up path alias

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@
  * @Author: Changwei Cao
  * @Date: 2024-10-23 20:17:15
  * @LastEditors: Changwei Cao
- * @LastEditTime: 2024-10-24 19:44:34
+ * @LastEditTime: 2024-10-25 20:12:37
  * @Description: 
  */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "reset-css"
-import "./assets/styles/global.scss"
+import "@/assets/styles/global.scss"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
